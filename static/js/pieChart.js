0b845112234fc787120dede5bc74f657aed348ae
Yuva38 <khanalsubodh@gmail.com>
2020-01-30 18:32:50 -0500

    first commit

---
+++ static/js/pieChart.js
@@ -0,0 +1,25 @@
+function populatePieChart(name, myJsonObj) {
+    let samples = myJsonObj.samples.filter(function (d) {
+        return d.id == name;
+    })
+
+    let count = 0;
+    // https://stackoverflow.com/questions/34883068/how-to-get-first-n-number-of-elements-from-an-array
+    let values = samples[0].sample_values.slice(0, 9);
+    let labels = samples[0].otu_ids.slice(0, 9);
+
+
+    var data = [{
+        values: values,
+        labels: labels,
+        hole: .4,
+        type: 'pie'
+    }];
+
+    var layout = {
+        height: 400,
+        width: 500
+    };
+
+    Plotly.newPlot('bar', data, layout);
+}//populatePieChart// JavaScript source code
